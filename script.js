f1fe051aea22f5f5fe40ae6cb8e8e0fd9b537177
liammccon <liam.t.mccon@gmail.com>
2023-02-16 21:47:59 -0500

    Add logic to quiz

--- script.js
+++ script.js
@@ -6,12 +6,8 @@ $(document).ready(function() {
     let currentQuestion = 0; //The current question, 0 is before starting the quiz
     let numOfCorrectAnswers = 0;
 
-    const answers = {
-        q1: 'a',
-        q2: 'c',
-        q3: 'b',
-        q4: 'a'
-    };
+
+    
 
     beginQuizBtn.click(function() {
         //Begin the Quiz
@@ -19,14 +15,58 @@ $(document).ready(function() {
         $('#quiz-questions').removeClass("d-none"); // Show the questions
 
         currentQuestion = 1;
+        populateQuiz(1);
     });
 
+    const questions = {
+        q1: {
+            question: "What is Nala's full name?",
+            a: "Nala M'Lady Nestor-Cuevas",
+            b: "Nala Schmala Bo Bala",
+            c: "Nala Lalalala La",
+            d: "Cat",
+            correct: "a"
+        },
+        q2: {
+            question: "Which is not incorrect?",
+            a: "False",
+            b: "Correct",
+            c: "False",
+            d: "False",
+            correct: "b"
+        }
+    }
+    
+    function populateQuiz(question){
+        
+    }
+
+    //Clicked on an answer button for the question
     quizAnswerBtn.click(function() {
         //Evaluate if answer was correct
-
+        const id = $(this).attr("id"); //ID can be a, b, c, or d.
+        evaluateAnswer(id);
     });
+    
+    function evaluateAnswer(answer){
+        const chosenAnswer = $(`#${answer}`);
 
+    }
 
+    //Return the currrent question object to display
+    function getCurrentQuestion {
+        switch (currentQuestion){
+            case 1:
+                return questions.q1;
+            case 2:
+                return questions.q2;
+        }
+    }
 
+    
 
+    
 });
+
+
+
