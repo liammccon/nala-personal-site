702afb70f079d5279a72d2a82f60c76496ccf0e0
liammccon <liam.t.mccon@gmail.com>
2023-02-16 20:20:16 -0500

    Add JQuery

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 $(document).ready(function() {
 
     // Select the HTML element with the ID "my-button"
-    var myButton = $('#my-button');
+    var beginBtn = $('#begin');
   
     // Add a click event listener to the button
-    myButton.click(function() {
+    beginBtn.click(function() {
       // Do something when the button is clicked
+        $("#begin-div").addClass("d-none"); // Hide the div
+        console.log("HIGDIHSFID");
     });
   
-  });
+});
