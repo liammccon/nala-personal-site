de8abe23864a5cecaca5671afb601c6290fcb72e
liammccon <liam.t.mccon@gmail.com>
2023-02-16 20:37:06 -0500

    Add javascript for quiz

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 // Wait for the HTML document to finish loading before running any jQuery code
 $(document).ready(function() {
 
-    // Select the HTML element with the ID "my-button"
-    var beginBtn = $('#begin');
-  
-    // Add a click event listener to the button
-    beginBtn.click(function() {
-      // Do something when the button is clicked
-        $("#begin-div").addClass("d-none"); // Hide the div
-        console.log("HIGDIHSFID");
+    let beginQuizBtn = $('#begin'); //Button to begin quiz
+    let quizAnswerBtn = $('.quiz-btn'); //All four buttons with possible answers
+    let currentQuestion = 0; //The current question, 0 is before starting the quiz
+    let numOfCorrectAnswers = 0;
+
+    const answers = {
+        q1: 'a',
+        q2: 'c',
+        q3: 'b',
+        q4: 'a'
+    };
+
+    beginQuizBtn.click(function() {
+        //Begin the Quiz
+        $("#begin-div").addClass("d-none"); // Hide the begin button
+        $('#quiz-questions').removeClass("d-none"); // Show the questions
+
+        currentQuestion = 1;
     });
-  
+
+    quizAnswerBtn.click(function() {
+        //Evaluate if answer was correct
+
+    });
+
+
+
+
 });
