7f4a7106a90b40209923e71cbaa5f011f7640f6e
liammccon <liam.t.mccon@gmail.com>
2023-02-16 20:09:47 -0500

    Add quiz template

---
+++ script.js
@@ -0,0 +1,12 @@
+// Wait for the HTML document to finish loading before running any jQuery code
+$(document).ready(function() {
+
+    // Select the HTML element with the ID "my-button"
+    var myButton = $('#my-button');
+  
+    // Add a click event listener to the button
+    myButton.click(function() {
+      // Do something when the button is clicked
+    });
+  
+  });
