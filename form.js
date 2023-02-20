bcf1fdaa2082c319c49863eb625ecf0481b8e806
liammccon <liam.t.mccon@gmail.com>
2023-02-19 22:04:49 -0500

    Add validity message

--- form.js
+++ form.js
@@ -1,5 +1,36 @@
 $(document).ready(function() { 
-    
+
+    $('#send-button').click(function() {
+        validate();
+    });
+    const inputIDs = ['#name', '#email', '#message'];
+
+
+    function validate() {
+        let allValid = true;
+        const success = $('#send-success');
+        success.addClass('d-none');
+
+        inputIDs.forEach( inputId => {
+            //Reset validation
+            const input = $(inputId);
+            const feedback = $(inputId+'-feedback');
+
+            input.removeClass('invalid');
+            feedback.addClass('d-none');
+
+            //If input contains no text
+            if(!input.val()){
+                allValid = false;
+                input.addClass('invalid');
+                feedback.removeClass('d-none');
+            }
+        });
+
+        if (allValid){
+            success.removeClass('d-none');
+        }
+    }
 });
 
 
